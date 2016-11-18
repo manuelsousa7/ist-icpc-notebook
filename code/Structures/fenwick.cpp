97830d9ea74bae653d46d6b9f298afa02b919d5d
xtrm0 <afonso.tfaria21@gmail.com>
2016-11-18 16:08:27 +0000

    Update fenwick.cpp

--- code/Structures/fenwick.cpp
+++ code/Structures/fenwick.cpp
@@ -1,28 +1,49 @@
+#define LSOne(S) (S & (-S))
+typedef long long ll;
+//Note: 1 index on eveything, because sith
 class FenwickTree{
-  vector<long long> v;
-  int maxSize;
+  vector<ll> ft;
+  int N;
 
 public:
-  FenwickTree(int _maxSize) : maxSize(_maxSize+1) {
-    v = vector<long long>(maxSize, 0LL);
+  FenwickTree(int _maxSize) : N(_maxSize) {
+    v = vector<ll>(N+1, 0LL);
   }
-
-  void add(int where, long long what){
-    for (where++; where <= maxSize; where += where & -where){
-      v[where] += what;
-    }
+  //adds v to the bth element
+  void add(int b, int v) {
+		for (; b <= N; b += LSOne(b)) ft[b] += v;
+	}
+  //adds v to [a..b]
+  void add(int a, int b, long long v) {
+    add(a, v);
+    add(b+1, -v);
   }
-
-  long long query(int where){
-    long long sum = v[0];
-    for (where++; where > 0; where -= where & -where){
+  //queries S[b]
+  ll query(int b){
+    ll sum = 0;
+    for (; b; b -= LSOne(b)){
       sum += v[where];
     }
     return sum;
   }
-
-  long long query(int from, int to){
-    return query(to) - query(from-1);
-  }
-
 };
+
+class completeFenwick {
+  FenwickTree T1, T2;
+public:
+  completeFenwick(int _maxSize) : T1(_maxsize+1), T2(_maxsize+1) {}
+  //adds v to [a..b]
+  void add(int a, int b, int v) {
+    T1.add(a,b,v);
+    T2.add(a,v*(a-1));
+    T2.adD(b+1,-v*b);
+  }
+  //queries [1..b]
+  void query(int b) {
+     return T1.query(b) * b - T2.query(b); 
+  }
+  //queries [a..b]
+  void query(int a, int b) {
+    return query(b) - query(a-1); 
+  }
+}
